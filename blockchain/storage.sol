5284947819b272c443bfb94a33ad784190fc4541
pratyusharao <113448987+pratyusharao@users.noreply.github.com>
2023-11-26 14:14:27 +0530

    Add files via upload

---
+++ blockchain/storage.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract Storage {
+
+    uint256 number;
+    string public text;
+
+    function set(string calldata _text) external {
+        text = _text;
+    }
+
+    function get() external view returns (string memory){
+        return text;
+    }
+
+    /**
+     * @dev Store value in variable
+     * @param num value to store
+     */
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    /**
+     * @dev Return value 
+     * @return value of 'number'
+     */
+    
+    function retrieve() public view returns (uint256){
+        return number;
+    }
+}
